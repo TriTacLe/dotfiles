6a73ce1ab2f9fb9e2ff76d1f19ba2eaa90b665c8
TriTacLe <tritac.le@gmail.com>
2026-08-05 15:19:13 +0200

    chore: treat main.c1-c6 as C, gitignore .cc-writes

--- shared/stow/nvim/.config/nvim/lua/config/options.lua
+++ shared/stow/nvim/.config/nvim/lua/config/options.lua
@@ -5,3 +5,6 @@ vim.g.lazyvim_python_lsp = "basedpyright"
 
 -- Builtin sql ftplugin hijacks <Right>/<Left>/<C-C> in insert mode (sqlcomplete drill maps)
 vim.g.omni_sql_no_default_maps = 1
+
+-- STM32 prac task files (main.c1..main.c6) are plain C
+vim.filetype.add({ pattern = { [".*%.c[1-6]"] = "c" } })
